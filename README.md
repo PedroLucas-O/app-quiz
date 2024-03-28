d53ecdc9aea57fa5f15ae5b5527c6d3ddcbdd1fe
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-03-28 17:11:44 -0300

    projeto de quiz

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1> Projeto de app-quiz </h1>
+<br> <br>
+<img src="./src/assets/img/projeto.png"/>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
 
+  public items:Array<any>=[
+    "ideia tem acento?",
+    "bem vindo tem hifen?",
+    "bau tem acento?",
+    "jiboia tem acento?",
+    "Aurelio tem acento?"
+  ]
+
   constructor() {}
 
+  reordenarItems(indexes:any):void {
+    let element =this.items[indexes.from];
+    this.items.splice(indexes.from,1);
+    this.items.splice(indexes.to,0,element)
+  }
+ btn(id:any):void {
+  alert("Você respondeu:" + id)
 }
+
+}
+
+
